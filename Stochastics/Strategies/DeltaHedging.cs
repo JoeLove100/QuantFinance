2616875cbf71c1f97309a4703e110d4d343ee6b3
Joe L <joelove100@gmail.com>
2020-05-18 07:04:14 +0100

    Add hedging strategy base class

---
+++ Stochastics/Strategies/DeltaHedging.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using OptionPricing;
+using Utilities.MarketData;
+
+namespace Stochastics.Strategies
+{
+    public class DeltaHedging : OptionHedgingStrategy
+    {
+        #region constructor
+
+        public DeltaHedging(EquityOption options, 
+                            int contractNumbers) : base(options, contractNumbers)
+        {
+
+        }
+
+        #endregion
+
+        #region overrides
+
+        public override SortedList<DateTime, double> GetDailyPnl(SortedList<DateTime, OptionPricingData> marketData)
+        {
+            var dailyPnl = new SortedList<DateTime, double>();
+            var firstDate = marketData.Keys[0];
+
+            var optionValue = _option.GetCurrentPrice();
+            
+        }
+
+        #endregion
+    }
+}
--- Stochastics/Strategies/OptionHedgingStrategy.cs
+++ Stochastics/Strategies/OptionHedgingStrategy.cs
@@ -3,10 +3,35 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using OptionPricing;
+using Utilities.MarketData;
 
 namespace Stochastics.Strategies
 {
-    class OptionHedgingStrategy
+    public abstract class OptionHedgingStrategy
     {
+        #region contructors
+
+        public OptionHedgingStrategy(EquityOption option,
+                                     int contracts)
+        {
+            _option = option;
+            _contractNumbers = contracts;
+        }
+
+        #endregion
+
+        #region properties
+
+        protected readonly EquityOption _option;
+        protected readonly int _contractNumbers;
+
+        #endregion
+
+        #region abstract methods
+
+        public abstract SortedList<DateTime,double> GetDailyPnl(SortedList<DateTime, OptionPricingData> marketData);
+
+        #endregion
     }
 }
